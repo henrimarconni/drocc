9765291743ea914c6364f7a4aa491a6884c85ae1
henrimarconni <297096622+henrimarconni@users.noreply.github.com>
2026-07-31 14:21:24 +0530

    Ported token_stream.c/h with rethinked interface
    Improved token_stream with a TS_FUNC type instead of different cluttered
    types like TS_MACRO_USE or whatever

---
+++ chucci_lex/src/chucci_lex/token_stream.h
@@ -0,0 +1,56 @@
+#ifndef __TOKEN_STREAM_H
+#define __TOKEN_STREAM_H
+
+#include "chucci_lex/token.h"
+#include "core/diagnostics.h"
+#include "core/vec.h"
+
+typedef Token (*TSNextFn)(void*);
+typedef Token (*TSPeekFn)(void*);
+typedef void (*TSFreeCtxFn)(void*);
+
+typedef enum TokenStreamKind {
+  TS_VEC,
+  /// Function based, like lex_next_token or pp_next_token
+  TS_FUNC,
+  /// Single token
+  TS_SINGLE,
+} TokenStreamKind;
+
+typedef struct {
+  void* ctx;
+  TSNextFn next;
+  TSPeekFn peek;
+  TSFreeCtxFn free;
+} TSFuncStream;
+
+typedef struct {
+  TokenVec vec;
+  size_t pos;
+} TSVecStream;
+
+typedef struct TokenStream {
+  TokenStreamKind kind;
+  bool is_consumed;
+  union {
+    TSVecStream vstream;
+    TSFuncStream fstream;
+    Token single;
+  };
+} TokenStream;
+
+typedef vec(TokenStream) TokenStreamStack;
+
+TokenStream ts_from_vec(TokenVec vec);
+TokenStream ts_from_token(Token token);
+Token ts_next(TokenStream* ts);
+Token ts_peek(TokenStream* ts);
+Token ts_expect(TokenStream* ts, TokenKind kind, DiagEngine* engine);
+Token tstack_expect(TokenStreamStack* stack, TokenKind kind, DiagEngine* engine);
+Token tstack_peek(TokenStreamStack* stack);
+Token tstack_next(TokenStreamStack* stack);
+
+void stream_free(TokenStream* ts);
+void tstack_free(TokenStreamStack* ts);
+
+#endif
