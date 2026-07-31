7d9eaa7a175c9c4ca0a3ecf9cce532b9208e0b40
henrimarconni <297096622+henrimarconni@users.noreply.github.com>
2026-07-31 11:48:45 +0530

    Fixed critical interning bugs and added tests for it
    Interner had following bugs:
    1. Arena aligns allocations by default, but interner was directly doing
    entry->offset = arena->pos, causing weird behaviour
    2. Redundant hash_str in resize function
    3. not copying string to its newly allocated memory in populate()
    4. bad logic in find_entry() linear probing
    
    Added interner_resize() test (which helped find these bugs)

--- core/src/string_interner.c
+++ core/src/string_interner.c
@@ -32,11 +32,12 @@ static inline InternID populate(InternEntry* entry, StringInterner* interner, bs
                                 size_t len) {
   entry->is_full = true;
   entry->char1 = str[0];
-  entry->is_full = 1;
-  entry->offset = interner->arena->pos;
   entry->intern_id = interner->len++;
 
   bstr new_str = vmarena_alloc(interner->arena, len + 1);
+  entry->offset = (size_t)(new_str - (char*)interner->arena->data);
+
+  memcpy(new_str, str, len);
   new_str[len] = '\0';
   return entry->intern_id;
 }
@@ -50,11 +51,14 @@ static inline InternEntry* find_entry(StringInterner* interner, bstr str, size_t
     return entry;
 
   // Already interned
-  if (str[0] == entry->char1 && strcmp(str + 1, entry_str(entry, interner) + 1))
+  if (str[0] == entry->char1 && strncmp(str + 1, entry_str(entry, interner) + 1, len - 1) == 0)
     return entry;
 
   // linear probing
-  while (!entry->is_full) {
+  while (entry->is_full) {
+    if (str[0] == entry->char1 && strncmp(str + 1, entry_str(entry, interner) + 1, len - 1) == 0)
+      return entry;
+
     id = wrap_around(id + 1, interner->cap);
     entry = &interner->entries.get[id];
   }
@@ -65,13 +69,16 @@ void resize(StringInterner* interner) {
   InternEntryVec old_entries = interner->entries;
   interner->entries = (InternEntryVec){0};
   interner->cap *= 2;
+
   vec_resize(interner->entries, interner->cap);
+  memset(interner->entries.get, 0, interner->entries.n * sizeof(InternEntry));
 
   for (size_t i = 0; i < old_entries.n; i++) {
     InternEntry* old_entry = &old_entries.get[i];
+    if (!old_entry->is_full)
+      continue;
     bstr str = entry_str(old_entry, interner);
     size_t len = strlen(str);
-    uint64_t hash = hash_str(str, len);
 
     InternEntry* new_entry = find_entry(interner, str, len);
     *new_entry = *old_entry;
