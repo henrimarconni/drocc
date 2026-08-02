1ffe052aa29ea1727b21bae12081d21442c6f74c
henrimarconni <297096622+henrimarconni@users.noreply.github.com>
2026-08-02 11:00:21 +0530

    Fixed code breakage in amalgamator due to SourceManager and fixed memory leaks
    Amalgamator had memory leaks when diagnostics were thrown.

--- amalgamator/src/amalgamate.h
+++ amalgamator/src/amalgamate.h
@@ -4,10 +4,22 @@
 #include "core/stringbuilder.h"
 #include "core/vmem_arena.h"
 #include <setjmp.h>
+#include "core/srcman.h"
+#include "core/diagnostics.h"
 
 typedef vec(bstr) IncludeDirVec;
 typedef vec(bstr) InputFIleVec;
 
-StringBuilder amalgamate(VMEMArena* arena, IncludeDirVec idirs, InputFIleVec input_files, jmp_buf* onerror);
+typedef struct {
+  StringBuilder output;
+  IncludeDirVec idirs;
+  InputFIleVec files;
+  SourceManager sman;
+  VMEMArena* arena;
+  DiagEngine engine;
+} Amalgamator;
+
+StringBuilder amalgamate(Amalgamator* a, jmp_buf* onerror);
+void amalgamator_free(Amalgamator* a);
 
 #endif
