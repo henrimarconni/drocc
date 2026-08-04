da0ac51427ae945275f3ddb029806a0afcd4ca72
henrimarconni <297096622+henrimarconni@users.noreply.github.com>
2026-08-04 21:31:25 +0530

    Add mmtok.c/h
    The parser for macromancer was ugly because it did both lexing and
    parsing (I earlier felt that it was faster), I separated them and added
    a new MMToken struct.

---
+++ macromancer/src/mmtok.c
@@ -0,0 +1,197 @@
+#include "core/diagnostics.h"
+#include "core/scanner.h"
+#include "core/span.h"
+#include "core/srcman.h"
+#include "core/stringdef.h"
+#include "macromancer/mm_diag.h"
+#include "macromancer/mmtok.h"
+#include "macromancer/parser.h"
+#include <assert.h>
+#include <ctype.h>
+#include <string.h>
+
+void skip_comment(SrcScanner* scanner) {
+  if (peekch(scanner) == '#') {
+    while (peekch(scanner) != '\n' && peekch(scanner) != EOF) {
+      nextch(scanner);
+    }
+  }
+}
+
+void skip_unwanted(SrcScanner* scanner) {
+  int last_id;
+  do {
+    last_id = scanner->id;
+    skip_comment(scanner);
+    skip_space(scanner);
+  } while (scanner->id != last_id && peekch(scanner) != EOF);
+}
+
+MMToken tok_ident(MMParser* p) {
+  Span span = span_begin(&p->scanner);
+
+  assert(nextch(&p->scanner) != EOF);
+  char ch = peekch(&p->scanner);
+  while (ch != EOF && (isalnum(ch) || ch == '_')) {
+    nextch(&p->scanner);
+    ch = peekch(&p->scanner);
+  }
+
+  span_end(&span, &p->scanner);
+
+  MMToken tok = {0};
+  tok.type = MMT_IDENT;
+  tok.span = span;
+  tok.sv = span_sv(p->sman, span);
+
+  if (memcmp(tok.sv.str, "as", 2) == 0)
+    tok.type = MMT_AS;
+
+  return tok;
+}
+
+MMToken tok_surround(MMParser* p, char start, char end) {
+  Span span = span_begin(&p->scanner);
+  assert(nextch(&p->scanner) == start);
+
+  int ch = peekch(&p->scanner);
+  while (ch != end && ch != EOF) {
+    nextch(&p->scanner);
+    ch = peekch(&p->scanner);
+  }
+
+  assert(ch == end);
+  assert(nextch(&p->scanner) == end);
+  span_end(&span, &p->scanner);
+
+  MMToken tok = {0};
+  tok.span = span;
+  tok.sv = span_sv(p->sman, span);
+
+  tok.sv.len -= 2;
+  tok.sv.str++;
+
+  return tok;
+}
+
+void expect_str(MMParser* p, bstr str) {
+  MMToken tok = get_mmtok(p);
+  size_t len = strlen(str);
+
+  assert(tok.sv.len == len);
+  assert(strncmp(tok.sv.str, str, len) == 0);
+}
+
+MMToken tok_angstr(MMParser* p) {
+  MMToken tok = tok_surround(p, '<', '>');
+  tok.type = MMT_ANGSTR;
+  return tok;
+}
+
+MMToken tok_str(MMParser* p) {
+  MMToken tok = tok_surround(p, '"', '"');
+  tok.type = MMT_STR;
+  return tok;
+}
+
+MMToken tok_keyw(MMParser* p) {
+  Span span = span_begin(&p->scanner);
+  assert(nextch(&p->scanner) == '$');
+
+  char ch = peekch(&p->scanner);
+  assert(isalpha(ch) || ch == '_');
+
+  while (ch != EOF && (isalnum(ch) || ch == '_')) {
+    nextch(&p->scanner);
+    ch = peekch(&p->scanner);
+  }
+
+  span_end(&span, &p->scanner);
+
+  MMToken tok = {0};
+  tok.type = MMT_KEYW;
+  tok.span = span;
+  tok.sv = span_sv(p->sman, span);
+
+  return tok;
+}
+
+MMToken tok_simple(MMParser* p, MMTokenType type) {
+  Span span = span_begin(&p->scanner);
+  nextch(&p->scanner);
+  span_end(&span, &p->scanner);
+
+  return (MMToken){
+      .type = type,
+      .span = span,
+      .sv = span_sv(p->sman, span),
+  };
+}
+
+MMToken get_mmtok(MMParser* p) {
+  skip_unwanted(&p->scanner);
+
+  int ch = peekch(&p->scanner);
+  assert(ch != EOF);
+
+  if (ch == '_' || isalpha(ch))
+    return tok_ident(p);
+
+  Span start_span = span_begin(&p->scanner);
+
+  if (match_str(&p->scanner, "$header")) {
+    MMToken tok = {0};
+    tok.type = MMT_HEADERPAIR;
+
+    skip_unwanted(&p->scanner);
+    expect_str(p, "=");
+    skip_unwanted(&p->scanner);
+
+    char next_c = peekch(&p->scanner);
+    if (next_c == '"')
+      tok.sv = tok_str(p).sv;
+    else if (next_c == '<')
+      tok.sv = tok_angstr(p).sv;
+    else
+      assert(false);
+
+    span_end(&start_span, &p->scanner);
+
+    tok.span = start_span;
+    tok.sv = span_sv(p->sman, start_span);
+    return tok;
+  }
+
+  switch (ch) {
+  case '=':
+    return tok_simple(p, MMT_EQ);
+  case '}':
+    return tok_simple(p, MMT_RCURLY);
+  case '{':
+    return tok_simple(p, MMT_LCURLY);
+  case '"':
+    return tok_str(p);
+  case '<':
+    return tok_angstr(p);
+  case '$':
+    return tok_keyw(p);
+  }
+
+  assert(false);
+}
+
+const bstr mmtok_type_to_str[] = {
+    "=", "{", "}", "STR", "ANGSTR", "KEYW", "IDENT", "HEADERPAIR", "PAIR", "AS"};
+
+MMToken expect_mmtok(MMParser* p, MMTokenType type) {
+  MMToken tok = get_mmtok(p);
+  if (tok.type != type) {
+    throw_diag(
+        &p->engine,
+        tok.span,
+        MM_ERR_UNEXPECTED_TOK,
+        mmtok_type_to_str[type],
+        mmtok_type_to_str[tok.type]);
+  }
+  return tok;
+}
