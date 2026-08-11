b5cf70a34f93bb5d2241b62cf35cad0c010d1473
henrimarconni <297096622+henrimarconni@users.noreply.github.com>
2026-08-11 11:13:44 +0530

    Added bool keyword

--- chucci_lex/src/chucci_lex/token.h
+++ chucci_lex/src/chucci_lex/token.h
@@ -93,6 +93,7 @@ extern const bool is_sep_table[256];
   X(KW_SIGNED, "signed")                                                                           \
   X(KW_DOUBLE, "double")                                                                           \
   X(KW_FLOAT, "float")                                                                             \
+  X(KW_BOOL, "bool")                                                                               \
   X(KW_CHAR, "char")
 
 #define is_tok_op_or_sep(token) (token.kind < TOK_EOF && token.kind > KW_SIZEOF)
