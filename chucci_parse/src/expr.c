0372017752da9a9b4fa3006c3cfb2a7c78f5a386
henrimarconni <297096622+henrimarconni@users.noreply.github.com>
2026-09-07 19:22:04 +0530

    Fixed some bugs in expr.c

--- chucci_parse/src/expr.c
+++ chucci_parse/src/expr.c
@@ -40,15 +40,89 @@ void print_expr(Parser* p, Expr* expr) {
   putchar(')');
 }
 
-static int binding_power(TokenKind kind) {
+static int prefix_binding_power(TokenKind kind) {
   switch (kind) {
-#define X(a, bp, _)                                                                                \
-  case a:                                                                                          \
-    return bp;
-    BINDING_POWERS(X)
-#undef X
+  case OP_ADD:     // +a
+  case OP_SUB:     // -a
+  case OP_NOT:     // !a
+  case OP_BIT_NOT: // ~a
+  case OP_BIT_AND: // &a
+  case OP_MUL:     // *a (dereference)
+  case OP_INC:     // ++a
+  case OP_DEC:     // --a
+    return 13;     // Unary prefix precedence
   default:
-    assert(false && "INVALID TOKEN");
+    return 0;
+  }
+}
+
+static int infix_binding_power(TokenKind kind) {
+  switch (kind) {
+  case OP_ASSIGN:
+  case OP_ADD_EQ:
+  case OP_SUB_EQ:
+  case OP_MUL_EQ:
+  case OP_DIV_EQ:
+  case OP_MOD_EQ:
+  case OP_SHL_EQ:
+  case OP_SHR_EQ:
+  case OP_AND_EQ:
+  case OP_OR_EQ:
+  case OP_XOR_EQ:
+    return 1;
+
+  case OP_QUESTION:
+  case OP_COLON:
+    return 2;
+
+  case OP_OR:
+    return 3;
+
+  case OP_AND:
+    return 4;
+
+  case OP_BIT_OR:
+    return 5;
+
+  case OP_BIT_XOR:
+    return 6;
+
+  case OP_BIT_AND:
+    return 7;
+
+  case OP_EQ:
+  case OP_NEQ:
+    return 8;
+
+  case OP_LE:
+  case OP_GE:
+  case OP_LT:
+  case OP_GT:
+    return 9;
+
+  case OP_SHL:
+  case OP_SHR:
+    return 10;
+
+  case OP_ADD:
+  case OP_SUB:
+    return 11;
+
+  case OP_MUL:
+  case OP_DIV:
+  case OP_MOD:
+    return 12;
+
+  case OP_INC:     // a++ (postfix)
+  case OP_DEC:     // a-- (postfix)
+  case OP_ARROW:   // a->b
+  case OP_DOT:     // a.b
+  case SEP_LPAREN: // f(...)
+  case SEP_LSQ:    // a[...]
+    return 14;
+
+  default:
+    return 0; // Returns 0 for delimiters (';', ')', ']', etc.) to safely break parse loop
   }
 }
 
@@ -62,7 +136,7 @@ static Expr* make_expr(Parser* p, ExprKind kind, void* data, size_t size) {
 static Expr* left_denotation(Parser* p, Expr* left, Token op) {
 #define data_size sizeof(TokenKind) + sizeof(Expr*) * 2
   if (is_binary[op.kind]) {
-    Expr* rhs = parse_expr_bp(p, binding_power(op.kind));
+    Expr* rhs = parse_expr_bp(p, infix_binding_power(op.kind));
 
     uint8_t data[data_size] = {0};
     *(TokenKind*)data = op.kind;
@@ -81,17 +155,17 @@ static Expr* null_denotation(Parser* p, Token token) {
     return make_expr(p, EXPR_PRIMARY, &token, sizeof(token));
 
   if (is_unary[token.kind]) {
-    Expr* inner = parse_expr_bp(p, binding_power(token.kind));
+    Expr* inner = parse_expr_bp(p, prefix_binding_power(token.kind));
 
     uint8_t data[data_size] = {0};
     *(TokenKind*)data = token.kind;
     *(Expr**)(data + sizeof(TokenKind)) = inner;
 
-    return make_expr(p, EXPR_UNARY, &data, data_size);
+    return make_expr(p, EXPR_UNARY, data, data_size);
   }
 
   if (token.kind == SEP_LPAREN) {
-    Expr* expr = parse_expr_bp(p, binding_power(token.kind));
+    Expr* expr = parse_expr_bp(p, 0);
     // TODO: ts_expect
     Token next = ts_next(&p->ts);
     assert(next.kind == SEP_RPAREN);
@@ -110,7 +184,7 @@ static Expr* parse_expr_bp(Parser* p, int bp) {
 
   Token peeked = ts_peek(&p->ts);
 
-  while (peeked.kind != TOK_EOF && binding_power(peeked.kind) > bp) {
+  while (peeked.kind != TOK_EOF && infix_binding_power(peeked.kind) > bp) {
     Token next_op = ts_next(&p->ts);
     // Handle infix/postfix operators (like +, *, or a function call)
     left = left_denotation(p, left, next_op);
